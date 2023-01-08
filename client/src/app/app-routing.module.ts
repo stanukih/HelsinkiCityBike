55023a51ad2a1b771b7e105d0545bea3b4b871b5
stanukih <stanukih@gmail.com>
2023-01-08 15:31:18 +0200

    Added page Login and Registration.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from 'path';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { BasicLayoutComponent } from './shared/layouts/basic-layout/basic-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: AuthLayoutComponent, children:[{
+      path:"login", component:LoginPageComponent
+    },
+    {
+      path:"registration", component:RegistrationPageComponent
+    },
+    {
+      path:"", redirectTo: "/login", pathMatch: "full"
+    },
+      
+    ]
+  },
+  {
+    path: "", component: BasicLayoutComponent, children:[
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { BasicLayoutComponent } from './shared/layouts/basic-layout/basic-layout.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    LoginPageComponent,
+    BasicLayoutComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
